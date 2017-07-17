8d70ee329f021a25fd009f5d496567c813721218
Vincent Chu <vince.chu@live.ca>
2017-07-17 19:22:47 -0400

    fix(startup): fix ENABLE_PRIVATE_COMMANDS switch

--- scripts/lib/startup.sh
+++ scripts/lib/startup.sh
@@ -52,7 +52,7 @@ boot()
 {
   loginit
   foreach load_modules ${DATA_PATH}/modules/builtins 1> /dev/null
-  foreach load_commands ${DATA_PATH}/commands/builtins $([[ $ENABLE_PRIVATE_COMMANDS ]]; ifelse "${DATA_PATH}/commands/private") 1> /dev/null
+  foreach load_commands ${DATA_PATH}/commands/builtins $([[ $ENABLE_PRIVATE_COMMANDS == true ]]; ifelse "${DATA_PATH}/commands/private") 1> /dev/null
   ifverb verb echo_modules_info
   ifverb verb echo_commands_info
   ifverb info echo
