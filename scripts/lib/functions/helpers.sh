5fb3f171866c105e86fcf1e6fad0ddd0fe499b5f
Vincent Chu <vince.chu@live.ca>
2017-07-18 13:37:45 -0400

    feat(foreach): add NO_SEPARATOR switch

--- scripts/lib/functions/helpers.sh
+++ scripts/lib/functions/helpers.sh
@@ -15,7 +15,7 @@ foreach()
   local i
   local -a elements=("${@:2}")
   for ((i = 0; i < "${#elements[@]}"; i++)); do
-    [[ $i -gt 0 ]] && echo -n "${SEPARATOR:- }"
+    [[ ! $NO_SEPARATOR && $i -gt 0 ]] && echo -n "${SEPARATOR:- }"
     "$1" "${elements[$i]}"
   done
 }
