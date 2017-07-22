0f988eb4f24da82273d95f4ec2d18aff2202ba91
Vincent Chu <vince.chu@live.ca>
2017-07-22 19:04:20 -0400

    feat(modules): add node image

---
+++ modules/node/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Docker Entrypoint Script
+#
+# This script executes some commands on Docker container
+# startup prior to the main command is executed.
+#
+
+#
+# Ensure that the NPM packages for the current project are 
+# installed. If the `node_modules` directory already exists, 
+# skips this step.
+#
+if [[ -e ./package.json && ! -d ./node_modules ]]; then
+  if [[ "$NPM_NO_BIN_LINKS" == true ]]; then
+    npm install --no-bin-links
+  else
+    npm install
+  fi
+fi
+
+# Fix missing node-sass vendor bug
+if [[ -d ./node_modules/node-sass && ! -d ./node_modules/node-sass/vendor ]]; then
+  if [[ "$NPM_NO_BIN_LINKS" == true ]]; then
+    npm rebuild node-sass --no-bin-links
+  else
+    npm rebuild node-sass
+  fi
+fi
+
+# Execute the main command
+exec "$@"
